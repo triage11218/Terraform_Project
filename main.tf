2478e14c35fcd0653af944a151c2cc323302687d
triage11218 <152022079+triage11218@users.noreply.github.com>
2024-03-18 21:58:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "RHEL-Server" {
+resource "aws_instance" "RHEL-VM-Sever" {
   count         = 1
   ami           = "ami-0fe630eb857a6ec83"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name      = "Triage_KeyPair"
   tags = {
     Name = "RHEL-Server"
   }
   root_block_device {
-    volume_size = 15 
+    volume_size = 12 
     volume_type = "gp3"
     encrypted   = true
   }
